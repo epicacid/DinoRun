b92264d63ebbae06cdb09a669627ccc58b9e8729
epicacid <noahsenteur12345@gmail.com>
2016-09-22 09:37:07 +0200

    Collision

---
+++ Assets/Scenes/NoahTestScene/Scripts/Collision.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collision : MonoBehaviour {
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.tag == "Obstakel") {
+			//Leven eraf 
+			Debug.Log ("Geraakt");
+		}
+	}
+}
