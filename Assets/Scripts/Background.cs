7364432c38aee4b0e6f13c1b8b159a8c0cd4f080
nielsvdmarel <nielsvdmarel@gmail.com>
2016-09-26 09:32:47 +0200

    Background Script werkend in speelbare scene

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -2,20 +2,18 @@
 using System.Collections;
 public class Background : MonoBehaviour {
 	Vector3 background;
-	[SerializeField]
 	private float _x;
-	[SerializeField]
-	private float _y;
+
 	[SerializeField]
 	private float _width = 15.0f;
-	[SerializeField]
-	private float _height = 15.0f;
+
+
 	void OnBecameInvisible()
 	{
 		background = gameObject.transform.position;
 		_x = background.x + _width * 2;
-		_y = background.y + _height * 2;
-		gameObject.transform.position = new Vector3 (_x, _y, 0f);
+	
+		gameObject.transform.position = new Vector3 (_x, 0f, 0f);
 	}
 }
 
