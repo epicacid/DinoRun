d85ab4854270aaf586c562951b8fc0c1408b8391
epicacid <noahsenteur12345@gmail.com>
2016-09-28 13:32:57 +0200

    Automovement obstacle

---
+++ Assets/Scenes/NoahTestScene/Scripts/AutoMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoMovement : MonoBehaviour {
+
+	Transform Obstacle;
+	[SerializeField]
+	private float _speed = 5;
+	private Vector2 _newPosition = new Vector2 (4,0);
+
+
+
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//transform.position = Vector2.MoveTowards (new Vector (transform.position.x), _newPosition, _speed * Time.deltaTime);
+		transform.Translate (Vector2.left * Time.deltaTime * _speed);
+
+
+
+			
+			
+	
+	}
+}
+
--- Assets/Scenes/NoahTestScene/Scripts/Collision.cs
+++ Assets/Scenes/NoahTestScene/Scripts/Collision.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Collision : MonoBehaviour {
 	void OnCollisionEnter2D(Collision2D coll)
 	{
-		if (coll.gameObject.tag == "Obstakel") {
+		if (coll.gameObject.tag == "Obstacle") {
 			//Leven eraf 
 			Debug.Log ("Geraakt");
 		}
