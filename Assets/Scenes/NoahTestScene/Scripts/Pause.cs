f71a60b1648615f623b9c89aeba651bdca86f5d7
epicacid <noahsenteur12345@gmail.com>
2016-09-28 14:11:04 +0200

    Pause Scherm

---
+++ Assets/Scenes/NoahTestScene/Scripts/Pause.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+	public bool pauseMenuEnabled = false;
+
+	void Update()
+	{
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			pauseMenuEnabled = !pauseMenuEnabled;
+		}
+		if (pauseMenuEnabled) {
+			Debug.Log("is paused");
+			Time.timeScale = 0.0f;
+			Debug.Log (Time.timeScale);
+		} else {
+			Debug.Log("is playing");
+			Time.timeScale = 1.0f;
+			Debug.Log (Time.timeScale);
+		}
+	}
+}
+
+
