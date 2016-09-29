dac74c15133dfbffdee47ca81b01d42e5f6268c3
epicacid <noahsenteur12345@gmail.com>
2016-09-29 09:49:15 +0200

    Obstacle

--- Assets/Scenes/NoahTestScene/Scripts/AutoMovement.cs
+++ Assets/Scenes/NoahTestScene/Scripts/AutoMovement.cs
@@ -2,27 +2,41 @@
 using System.Collections;
 
 public class AutoMovement : MonoBehaviour {
-
-	Transform Obstacle;
 	[SerializeField]
-	private float _speed = 1;
+	private Transform _obstacle;
+	[SerializeField]
+	private Transform _targetA;
+	[SerializeField]
+	private Transform _targetB;
+	[SerializeField]
+	private float _minDistance = 0.1f;
+	[SerializeField]
+	private float _EnemySpeed = 2.0f;
+	[SerializeField]
+	private float _rangeA;
+	[SerializeField]
+	private float _rangeB;
 
-	/*
-	void OnTriggerEnter2D(Collider2D coll)
+	public void FixedUpdate()
 	{
-		if (coll.gameObject.tag == "Right") {
-			Debug.Log ("Left");
-			transform.Translate (Vector2.left * Time.deltaTime * _speed);
-		} 
-		if (coll.gameObject.tag == "Left") {
-			Debug.Log ("Right");
-			transform.Translate (Vector2.right * Time.deltaTime * _speed);
+		_rangeA = Vector2.Distance (transform.position, _targetA.position); //Vector2.distance tussen A en B dus Enemy en Player de range slaat de waardes van de Position van Enemy en Player.
+		_rangeB = Vector2.Distance (transform.position, _targetB.position);
+
+		if (_rangeA < _minDistance) { // Als range kleiner is als minDistance begint hij hem te volgen.
+			Debug.Log (_rangeA);
+			transform.position = Vector2.MoveTowards (transform.position, _targetA.position, _EnemySpeed * Time.deltaTime);
+			} 
+
 		}
-	}
-	*/
 
 	
-}
+	}
+
+
+
+
+
+
 
 
 
