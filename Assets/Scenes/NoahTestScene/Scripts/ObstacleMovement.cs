ae40b823a7699ef573a8c671188fec3666e15b62
epicacid <noah@Noahs-MacBook-Pro.local>
2016-10-03 11:20:21 +0200

    autoMovement

--- Assets/Scenes/NoahTestScene/Scripts/ObstacleMovement.cs
+++ Assets/Scenes/NoahTestScene/Scripts/ObstacleMovement.cs
@@ -4,31 +4,23 @@ using System.Collections;
 public class ObstacleMovement : MonoBehaviour {
 
 	[SerializeField]
-	private float _minimumDist = -1.0f;
+	private float _minimumDist;
 	[SerializeField]
-	private float _maximumDist =  1.0f;
+	private float _maximumDist;
 	[SerializeField]
 	private float obstacleSpeed = 3.0f;
 	[SerializeField]
 	private float t = 0.0f;
 
-
-
-
-	
 	// Update is called once per frame
 	void Update () {
 		transform.position = new Vector3 (Mathf.Lerp (_minimumDist, _maximumDist, t ),0, 0);
-		t += 0.5f;
-
-
-		if (t > 0.5f) {
+		t += Time.deltaTime * obstacleSpeed;
+		if (t > 1f) {
 			float temp = _maximumDist;
 			_maximumDist = _minimumDist;
 			_minimumDist = temp;
 			t = 0.0f;
-
 		}
-
 	}
 }
