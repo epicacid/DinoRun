2057a01a5d1e8c6b3d9fe2488f369c1d9befed72
nielsvdmarel <nielsvdmarel@gmail.com>
2016-10-13 10:04:21 +0200

    player health v1

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+
+    public float maxHealth;
+    public float minHealth;
+
+    public float currentHealth;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+
+        if (currentHealth < minHealth)
+        {
+            Destroy(gameObject);
+
+
+
+
+
+        }
+    }
+
+}
